77653fd33d267df9f832c6daf95464708ac3d0b2
jabbcode <jabbsontouch@gmail.com>
2022-09-05 17:58:55 -0400

    modificando el index, agregandole el cors al servidor

--- models/server.js
+++ models/server.js
@@ -4,11 +4,12 @@ const http = require('http')
 const socketio = require('socket.io')
 const path = require('path')
 const Sockets = require('./sockets')
+const cors = require('cors')
 
 class Server {
     constructor() {
         this.app = express()
-        this.port = process.env.PORT || 4000
+        this.port = process.env.PORT
 
         // Http server
         this.server = http.createServer(this.app)
@@ -20,6 +21,9 @@ class Server {
     middlewares() {
         // Desplegar el directorio publico
         this.app.use(express.static(path.resolve(__dirname, '../public')))
+
+        // CORS
+        this.app.use(cors())
     }
 
     configurarSockets() {
